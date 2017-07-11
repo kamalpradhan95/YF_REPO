39bac54a735dfc804ce4a2168848706d6ee8cc65
kamalpradhan95 <kamalpradhan95@users.noreply.github.com>
2017-07-11 11:18:00 +0530

    Add files via upload

---
+++ yf_embedd.sql
@@ -0,0 +1,30 @@
+INSERT INTO yellowfingeo.yf_embedd (uid,Categories,Amount,Month_Year,`Year`,Num,Month_of_Date) VALUES 
+('Ravi','Healthcare',30000,'Jan-16',2016,1,'2016-01-01')
+,('Ravi','Shopping',30000,'Jan-16',2016,1,'2016-01-01')
+,('Ravi','Restaurant',20000,'Jan-16',2016,1,'2016-01-01')
+,('Ravi','Entertainment',15000,'Feb-16',2016,2,'2016-02-01')
+,('Ravi','Miscellaneous',10000,'Mar-16',2016,3,'2016-03-01')
+,('Ravi','Auto Fuel',15000,'Apr-16',2016,4,'2016-04-01')
+,('Ravi','Healthcare',30000,'May-16',2016,5,'2016-05-01')
+,('Ravi','Shopping',15000,'May-16',2016,5,'2016-05-01')
+,('Ravi','Restaurant',10000,'May-16',2016,5,'2016-05-01')
+,('Samir','Entertainment',8000,'May-16',2016,5,'2016-05-01')
+;
+INSERT INTO yellowfingeo.yf_embedd (uid,Categories,Amount,Month_Year,`Year`,Num,Month_of_Date) VALUES 
+('Ravi','Miscellaneous',10000,'Jun-16',2016,6,'2016-06-01')
+,('Samir','Auto Fuel',5000,'Jun-16',2016,6,'2016-06-01')
+,('Samir','Healthcare',40000,'Jan-17',2017,7,'2017-01-01')
+,('Samir','Shopping',20000,'Jan-17',2017,7,'2017-01-01')
+,('Samir','Restaurant',10000,'Jan-17',2017,7,'2017-01-01')
+,('Samir','Entertainment',10000,'Feb-17',2017,8,'2017-02-01')
+,('Ravi','Miscellaneous',12000,'Mar-17',2017,9,'2017-03-01')
+,('Samir','Auto Fuel',10000,'Apr-17',2017,10,'2017-04-01')
+,('Samir','Healthcare',40000,'May-17',2017,11,'2017-05-01')
+,('Samir','Shopping',15000,'May-17',2017,11,'2017-05-01')
+;
+INSERT INTO yellowfingeo.yf_embedd (uid,Categories,Amount,Month_Year,`Year`,Num,Month_of_Date) VALUES 
+('Ravi','Restaurant',15000,'May-17',2017,11,'2017-05-01')
+,('Ravi','Entertainment',20000,'May-17',2017,11,'2017-05-01')
+,('Ravi','Miscellaneous',10000,'Jun-17',2017,12,'2017-06-01')
+,('Samir','Auto Fuel',10000,'Jun-17',2017,12,'2017-06-01')
+;
